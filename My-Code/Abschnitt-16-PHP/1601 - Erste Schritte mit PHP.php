d05882f3e4beabd8519cd51918383d97003bb36a
Igneeles <69846931+Igneele91@users.noreply.github.com>
2022-09-01 16:36:40 +0200

    change 4 folder strucktur comit
    comit 4 folder struc

--- My-Code/Abschnitt-16-PHP/1601 - Erste Schritte mit PHP.php
+++
@@ -1,7 +0,0 @@
-<h1>Mein PHP Dokument</h1>
-<div>
-	<?php 
-		echo 3+5;
-	
-	?>
-</div>
--- My-Code/Abschnitt-16-PHP/1610 - Include Befehl/const.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$mo = "Montag";
-	$di = "Dienstag";
-	$mi = "Mittwoch";
-	$do = "Donnerstag";
-	$fr = "Freitag";
-	$sa = "Samstag";
-	$so = "Sonntag";
-?>
